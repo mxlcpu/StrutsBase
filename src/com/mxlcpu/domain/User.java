2535510d031e08de6010a0211f30170258d449eb
mxlcpu <mxlcpu@gmail.com>
2018-09-13 17:05:42 +0800

    修改User

--- src/com/mxlcpu/domain/User.java
+++ src/com/mxlcpu/domain/User.java
@@ -3,5 +3,14 @@ package com.mxlcpu.domain;
 public class User {
 	private Integer id;
 	private String username;
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(Integer id, String username) {
+		super();
+		this.id = id;
+		this.username = username;
+	}
 	
 }
