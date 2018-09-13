3052453328f338f30faf27d5cf3d634bb9bdc258
mxlcpu <mxlcpu@gmail.com>
2018-09-13 20:17:03 +0800

    鞍山User

--- src/com/mxlcpu/domain/User.java
+++ src/com/mxlcpu/domain/User.java
@@ -9,5 +9,17 @@ public class User {
 		this.id = id;
 		this.username = username;
 	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
 	
 }
