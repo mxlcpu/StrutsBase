f478442797b955062e847745e8b7a4fd55cae2fb
mxlcpu <mxlcpu@gmail.com>
2018-09-13 17:09:46 +0800

    精简User

--- src/com/mxlcpu/domain/User.java
+++ src/com/mxlcpu/domain/User.java
@@ -3,10 +3,7 @@ package com.mxlcpu.domain;
 public class User {
 	private Integer id;
 	private String username;
-	public User() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+	public User() {}
 	public User(Integer id, String username) {
 		super();
 		this.id = id;
